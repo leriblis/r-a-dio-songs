c9fbc59addf5aea8087527f5cc709f70265b99af
leriblis <mishin1991+weeb@gmail.com>
2018-12-22 17:42:30 +0200

    22-12-18 update

--- autoupdate.sh
+++ autoupdate.sh
@@ -4,8 +4,10 @@ BASEDIR=$(dirname "$0")
 PYSCRIPT=${BASEDIR}/parse_radio.py
 LOGNAME=${BASEDIR}/$(date +"%d_%m_%y_parse.log")
 
-python $PYSCRIPT update &> $LOGNAME
 cd $BASEDIR
+xz -d songs_db.json.xz
+python $PYSCRIPT update &> $LOGNAME
+xz -z songs_db.json
 git add . --all
 git commit -m "$(date +"%d-%m-%y update")"
 git push -u origin master
